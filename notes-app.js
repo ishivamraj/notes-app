825fc8fbd84e82f9a8a5973675980c58d8a29bfd
shivamraj <68854315+shivamraj@users.noreply.github.com>
2020-12-27 09:05:36 +0530

    added local storage

--- notes-app.js
+++ notes-app.js
@@ -1,41 +1,57 @@
-const notes = [
-  {
-    title: "My next trip",
-    body: "I would like to go to Spain",
-  },
-  {
-    title: "Habbits to work on",
-    body: "Exercise. Eating a bit better.",
-  },
-  {
-    title: "Office modification",
-    body: "Get a new seat",
-  },
-];
+let notes = [];
 
 const filters = {
   searchText: "",
 };
+
+//Retrieving existing saved data data
+const notesJSON = localStorage.getItem("notes");
+if (notesJSON !== null) {
+  notes = JSON.parse(notesJSON);
+}
+
+// localStorage.setItem('location', 'Philadelphia')
+// console.log(localStorage.getItem('location'))
+// localStorage.removeItem('location')
+// localStorage.clear()
+// const user = {
+//   name: 'Andrew',
+//   age: 27
+// }
+// const userJSON = localStorage.getItem('user')
+// console.log(userJSON)
+// const user = JSON.parse(userJSON)
+// console.log(`Username is ${user.name} and age is ${user.age}`)
 
 const renderNotes = function (notes, filters) {
   const filteredNotes = notes.filter(function (note) {
     return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
   });
 
-  document.querySelector('#notes').innerHTML = ''
+  document.querySelector("#notes").innerHTML = "";
 
-  filteredNotes.forEach(function(note){
-      const noteEl = document.createElement('p');
-      noteEl.textContent = note.title
-      document.querySelector('#notes').appendChild(noteEl)
-  })
+  filteredNotes.forEach(function (note) {
+    const noteEl = document.createElement("p");
+    if (note.title.length > 0) {
+      noteEl.textContent = note.title;
+    } else {
+      noteEl.textContent = 'Unnamed Note'
+    }
+    document.querySelector("#notes").appendChild(noteEl);
+  });
 };
 
 renderNotes(notes, filters); //Initially app renders something
 
 document.querySelector("#create-note").addEventListener("click", function (e) {
-  console.log(e);
-  e.target.textContent = "The Button was clicked!";
+  notes.push({
+    title: "",
+    body: "",
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
+  renderNotes(notes, filters);
+  // console.log(e);
+  // e.target.textContent = "The Button was clicked!";
 });
 
 // document.querySelector("#remove-all").addEventListener("click", function () {
@@ -54,7 +70,6 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
 //   console.log(e.target.elements.firstName.value)
 //   e.target.elements.firstName.value = ''
 // })
-
-document.querySelector('#for-fun').addEventListener('change', function(e){
-  console.log(e.target.checked)
-})
+document.querySelector("#filter-by").addEventListener("change", function (e) {
+  console.log(e.target.value);
+});

ed7ebea5b59797a9343f459574d9069e4bf47acd
shivamraj <68854315+shivamraj@users.noreply.github.com>
2021-01-07 14:35:44 +0530

    add delete button

--- notes-functions.js
+++ notes-functions.js
@@ -11,12 +11,21 @@ const getSavedNotes = function () {
 
 //Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
-  const noteEl = document.createElement("p");
+  const noteEl = document.createElement("div");
+  const textEl = document.createElement('span')
+  const button = document.createElement('button')
+
+  //Set up the remove note button
+  button.textContent = 'x'
+  noteEl.appendChild(button)
+  
+  //Set up the note title text
   if (note.title.length > 0) {
-    noteEl.textContent = note.title;
+    textEl.textContent = note.title;
   } else {
-    noteEl.textContent = "Unnamed Note";
+    textEl.textContent = "Unnamed Note";
   }
+  noteEl.appendChild(textEl)
   return noteEl
 }
 
